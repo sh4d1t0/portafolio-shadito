e5136a695d65e65e2be8f6e97f0385c0f586e026
sh4d1t0 <shadito@live.com.mx>
2022-01-06 20:12:24 -0600

    add typescript gatsby-config with ts-node

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,3 @@
-module.exports = {
-  siteMetadata: {
-    siteUrl: 'https://sh4d1t0.github.io',
-    title: 'Portafolio Shadito',
-  },
-  plugins: [
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: `src`,
-        path: `${__dirname}/src/`,
-      },
-    },
-    'gatsby-plugin-postcss',
-    {
-      resolve: `gatsby-plugin-typescript`,
-      options: {
-        isTSX: true, // defaults to false
-        jsxPragma: `jsx`, // defaults to "React"
-        allExtensions: true, // defaults to false
-      },
-    },
-    'gatsby-transformer-remark',
-  ],
-}
+require('ts-node').register()
+
+module.exports = require('./gatsby-config.ts')
