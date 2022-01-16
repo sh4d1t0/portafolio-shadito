f615f42f50ab635c04bde296d7524e57e05a92af
sh4d1t0 <shadito@live.com.mx>
2022-01-16 05:09:06 -0600

    fix storybook

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,40 @@
-require('ts-node').register()
-
-module.exports = require('./gatsby-config.ts')
+module.exports = {
+  siteMetadata: {
+    siteUrl: 'https://sh4d1t0.github.io',
+    title: 'Portafolio Shadito',
+    description: 'Plantilla para proyectos',
+    author: 'Shadito',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-image',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Portafolio',
+        short_name: 'Portafolio',
+        start_url: '/',
+        background_color: '#f7f0eb',
+        theme_color: '#2563EB',
+        display: 'minimal-ui',
+        icon: 'src/images/icon.png',
+      },
+    },
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-typescript',
+    'gatsby-plugin-styled-components',
+    'gatsby-transformer-remark',
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // 'gatsby-plugin-offline',
+  ],
+}
