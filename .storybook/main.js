bad5222f4a43156df4cd13cbfb238aac0d826dce
sh4d1t0 <shadito@live.com.mx>
2022-01-16 13:38:55 -0600

    fix storybook with gatsby on windows 10

--- .storybook/main.js
+++ .storybook/main.js
@@ -15,7 +15,6 @@ module.exports = {
         },
       },
     },
-    'storybook-addon-gatsby',
     '@storybook/addon-jest',
     'tailwind-tales-addon',
   ],
@@ -26,10 +25,13 @@ module.exports = {
   webpackFinal: async config => {
     // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
     config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/]
+    // Remove core-js to prevent issues with Storybook
+    config.module.rules[0].exclude = [/core-js/]
     // Use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
     config.module.rules[0].use[0].options.plugins.push(
       require.resolve('babel-plugin-remove-graphql-queries')
     )
+    config.resolve.mainFields = ['browser', 'module', 'main']
     return config
   },
 }
